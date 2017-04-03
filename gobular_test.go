94d05e3273a4f7c944b9c850a86ff3aa421c5a36
Markus Teich <markus.teich@stusta.mhn.de>
2017-04-03 03:11:55 +0200

    simplify tests

--- gobular_test.go
+++ gobular_test.go
@@ -4,163 +4,44 @@ import (
 	"testing"
 )
 
-func testFit(mustLen uint32, align Alignment, s string) string {
-	return ansiEsc.ReplaceAllLiteralString(fitPad(mustLen, align, s), "")
+func testFit(t *testing.T, s string, must string, len uint32, align Alignment) {
+	got := ansiEsc.ReplaceAllLiteralString(fitPad(len, align, s), "")
+	if got != must {
+		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
+	}
 }
 
-// TODO: simplify by extracting single test function
 // TODO: test multibyte and/or double-width utf8 characters
 func TestFitPad(t *testing.T) {
-	s := "123456"
-	must := "123456"
-	if got := testFit(6, HAlignLeft, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
+	testFit(t, "123456", "123456", 6, HAlignLeft)
+	testFit(t, "123456", "123456", 6, HAlignCenter)
+	testFit(t, "123456", "123456", 6, HAlignRight)
+	testFit(t, "123456", "123456    ", 10, HAlignLeft)
+	testFit(t, "123456", "  123456  ", 10, HAlignCenter)
+	testFit(t, "123456", "    123456", 10, HAlignRight)
+	testFit(t, "123456", "123456     ", 11, HAlignLeft)
+	testFit(t, "123456", "  123456   ", 11, HAlignCenter)
+	testFit(t, "123456", "     123456", 11, HAlignRight)
+	testFit(t, "123456", "12", 2, HAlignLeft)
+	testFit(t, "123456", "34", 2, HAlignCenter)
+	testFit(t, "123456", "56", 2, HAlignRight)
+	testFit(t, "123456", "123", 3, HAlignLeft)
+	testFit(t, "123456", "345", 3, HAlignCenter)
+	testFit(t, "123456", "456", 3, HAlignRight)
 
-	must = "123456"
-	if got := testFit(6, HAlignCenter, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "123456"
-	if got := testFit(6, HAlignRight, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "123456    "
-	if got := testFit(10, HAlignLeft, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "  123456  "
-	if got := testFit(10, HAlignCenter, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "    123456"
-	if got := testFit(10, HAlignRight, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "123456     "
-	if got := testFit(11, HAlignLeft, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "  123456   "
-	if got := testFit(11, HAlignCenter, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "     123456"
-	if got := testFit(11, HAlignRight, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "12"
-	if got := testFit(2, HAlignLeft, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "34"
-	if got := testFit(2, HAlignCenter, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "56"
-	if got := testFit(2, HAlignRight, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "123"
-	if got := testFit(3, HAlignLeft, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "345"
-	if got := testFit(3, HAlignCenter, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "456"
-	if got := testFit(3, HAlignRight, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	s = "123\033[38;5;226m456"
-
-	must = "123456"
-	if got := testFit(6, HAlignLeft, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "123456"
-	if got := testFit(6, HAlignCenter, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "123456"
-	if got := testFit(6, HAlignRight, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "123456    "
-	if got := testFit(10, HAlignLeft, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "  123456  "
-	if got := testFit(10, HAlignCenter, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "    123456"
-	if got := testFit(10, HAlignRight, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "123456     "
-	if got := testFit(11, HAlignLeft, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "  123456   "
-	if got := testFit(11, HAlignCenter, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "     123456"
-	if got := testFit(11, HAlignRight, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "12"
-	if got := testFit(2, HAlignLeft, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "34"
-	if got := testFit(2, HAlignCenter, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "56"
-	if got := testFit(2, HAlignRight, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "123"
-	if got := testFit(3, HAlignLeft, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "345"
-	if got := testFit(3, HAlignCenter, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
-
-	must = "456"
-	if got := testFit(3, HAlignRight, s); got != must {
-		t.Errorf("Must:|%s| Got:|%s|\n", must, got)
-	}
+	testFit(t, "123\033[38;5;226m456", "123456", 6, HAlignLeft)
+	testFit(t, "123\033[38;5;226m456", "123456", 6, HAlignCenter)
+	testFit(t, "123\033[38;5;226m456", "123456", 6, HAlignRight)
+	testFit(t, "123\033[38;5;226m456", "123456    ", 10, HAlignLeft)
+	testFit(t, "123\033[38;5;226m456", "  123456  ", 10, HAlignCenter)
+	testFit(t, "123\033[38;5;226m456", "    123456", 10, HAlignRight)
+	testFit(t, "123\033[38;5;226m456", "123456     ", 11, HAlignLeft)
+	testFit(t, "123\033[38;5;226m456", "  123456   ", 11, HAlignCenter)
+	testFit(t, "123\033[38;5;226m456", "     123456", 11, HAlignRight)
+	testFit(t, "123\033[38;5;226m456", "12", 2, HAlignLeft)
+	testFit(t, "123\033[38;5;226m456", "34", 2, HAlignCenter)
+	testFit(t, "123\033[38;5;226m456", "56", 2, HAlignRight)
+	testFit(t, "123\033[38;5;226m456", "123", 3, HAlignLeft)
+	testFit(t, "123\033[38;5;226m456", "345", 3, HAlignCenter)
+	testFit(t, "123\033[38;5;226m456", "456", 3, HAlignRight)
 }
